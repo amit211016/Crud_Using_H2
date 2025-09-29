bc5abdcc2593e86ad730b6cfd1a3d0a46ca82c66
amit <amit211016cse@gmail.com>
2025-09-29 17:01:36 +0530

    operation/multiplication

--- src/main/java/com/Operation.java
+++ src/main/java/com/Operation.java
@@ -11,7 +11,7 @@ public class Operation {
         // Subtraction
 
         private int subtract(int a, int b) {
-            return a - b;
+            return (a - b);
         }
 
         // Multiplication
