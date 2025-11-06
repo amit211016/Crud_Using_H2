5f0f28873a236fe47f92e275c87121aa3baa287f
AMIT SINGH <amit211016cse@gmail.com>
2025-11-06 13:27:55 +0530

    Remove @Service annotation from StudentService interface

--- src/main/java/com/CrudUsingH2/service/StudentService.java
+++ src/main/java/com/CrudUsingH2/service/StudentService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface StudentService {
     public abstract StudentResponseDto findStudentById(Long id);
     public abstract List<StudentResponseDto> findAllStudent();
